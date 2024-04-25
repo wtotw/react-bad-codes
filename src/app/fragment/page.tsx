5498ab09df5fc46ce3da9f77eac56c4735357ff4
koguuum <koguum43wk@gmail.com>
2024-04-25 10:55:33 +0900

    fix 4

--- src/app/fragment/page.tsx
+++ src/app/fragment/page.tsx
@@ -16,10 +16,10 @@ function Component3() {
  */
 export default function Page() {
   return (
-    <div>
+    <>
       <Component1 />
       <Component2 />
       <Component3 />
-    </div>
+    </>
   );
 }
