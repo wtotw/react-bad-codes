094d88b3a44fa92a61e7a288cd220f5c598901b8
koguuum <koguum43wk@gmail.com>
2024-04-25 20:19:39 +0900

    fix 10

--- src/app/import-order/page.tsx
+++ src/app/import-order/page.tsx
@@ -1,10 +1,10 @@
 /**
  * importも可読性を意識しましょう。
  */
-// import { commonFunction } from '../../lib/functions';
 // import React from 'react';
-// import { Component } from './Component';
 // import { format } from 'date-fns';
+// import { Component } from './Component';
+// import { commonFunction } from '../../lib/functions';
 
 export default function Page() {
   return <>ほげ</>;
