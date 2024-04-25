d442762fb16dbf9fef15582b2adf0409523ae2e0
koguuum <koguum43wk@gmail.com>
2024-04-25 20:21:14 +0900

    fix 11

--- src/app/implicit-return/page.tsx
+++ src/app/implicit-return/page.tsx
@@ -2,9 +2,7 @@ export default function Page() {
   /**
    * 関数を暗黙のreturnで書くこともできます。
    */
-  const add = (a: number, b: number) => {
-    return a + b;
-  };
+  const add = (a: number, b: number) => a + b;
 
   return <>ほげ</>;
 }
