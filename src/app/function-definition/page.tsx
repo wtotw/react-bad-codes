da1b129c8a2487a24449d1a884ca717033123704
koguuum <koguum43wk@gmail.com>
2024-04-25 11:13:19 +0900

    fix 5

--- src/app/function-definition/page.tsx
+++ src/app/function-definition/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 /**
  * レンダーでの関数定義は最小限に抑えましょう。
@@ -8,10 +8,15 @@ import { useState } from 'react';
  */
 export default function Page() {
   const [count, setCount] = useState(0);
+
+  const onClick = useCallback(() => {
+    setCount(count + 1)
+  }, [count]);
+
   return (
     <div>
       <p>count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+1ボタン</button>
+      <button onClick={onClick}>+1ボタン</button>
     </div>
   );
 }
